b76a89abdad0c921cdbe356958e4eb89d35ab485
ansskf <anss.khaled00@gmail.com>
2020-11-03 18:02:34 +0200

    Try to vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <hr>
+  <EmployeeForm @add-employee="addEmployee" />
+  <hr>
+  <EmployeeTable :employees="employees" @delete-employee="deleteEmployee"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from './components/EmployeeTable.vue'
+import EmployeeForm from './components/EmployeeForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    EmployeeTable,
+    EmployeeForm
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    }
+  },
+  methods: {
+    addEmployee(employee) {
+    const lastId = this.employees.length > 0 ? this.employees[this.employees.length - 1].id : 0;
+    const id = lastId + 1;
+    const newEmployee = { ...employee, id };
+
+      this.employees = [...this.employees, newEmployee]
+    }
+  },
+
+  deleteEmployee(id) {
+    console.log(id);
+    this.employees = this.employees.filter( x => (x.id !== id))
+    console.log(this.employees)
   }
 }
 </script>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.email }}</td>
+          <button @click="() => deleteEmployee(employee.id)">Delete</button>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'employee-table',
+        props: {
+            employees: Array,
+        },
+
+        methods: {
+            deleteEmployee(id) {
+                console.log(id);
+                this.$emit('delete-employee', id)
+            }
+        }
+    }
+</script>
+
+<style scoped></style>
